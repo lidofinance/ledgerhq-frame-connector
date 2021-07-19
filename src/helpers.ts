17d2bd40381559ac8aac7731e49ba677296443cb
George Avsetsin <mail@sjors.ru>
2021-07-19 22:20:29 +0300

    fix: isLedgerDappBrowserProvider

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,13 +6,22 @@ export const isIframe = (): boolean => {
   }
 };
 
-export const isLedgerDappBrowserProvider = (): boolean => {
-  try {
-    const params = new URLSearchParams(window.self.location.search);
-    const isEmbed = !!params.get('embed');
+export const isLedgerDappBrowserProvider = (() => {
+  let state: boolean | null = null;
 
-    return isIframe() && isEmbed;
-  } catch (error) {
-    return false;
-  }
-};
+  return (): boolean => {
+    if (typeof state === 'boolean') return state;
+    if (typeof window === 'undefined') return false;
+
+    try {
+      const params = new URLSearchParams(window.self.location.search);
+      const isEmbed = !!params.get('embed');
+
+      state = isIframe() && isEmbed;
+    } catch (error) {
+      state = false;
+    } finally {
+      return !!state;
+    }
+  };
+})();
